a88b6b53610b270dfb5123de0cff2009d9c562a2
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-10-26 06:32:44 +0800

    finalize stack navigation pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,53 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { MessageSquarePlus, X } from 'lucide-react-native';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { Stack, Link } from "expo-router";
+import { TouchableOpacity } from "react-native";
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+import { ConvexReactClient, ConvexProvider } from "convex/react";
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+    unsavedChangesWarning: false
+});
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+export default function RootLayoutNav() {
+    return (
+        <ConvexProvider client={convex}>
+            <Stack
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: "#F9D949"
+                    },
+                    headerTintColor: "#3F3F3F"
+                }}>
+                <Stack.Screen
+                    name="index"
+                    options={{
+                        headerTitle: 'My Chats',
+                        headerRight: () => (
+                            <Link href={'/(modal)/create'} asChild>
+                                <TouchableOpacity>
+                                    <MessageSquarePlus/>
+                                </TouchableOpacity>
+                            </Link>
+                        ),
+                    }}
+                />
+                <Stack.Screen
+                    name="(modal)/create"
+                    options={{
+                        headerTitle: 'Start a Chat',
+                        presentation: 'modal',
+                        headerLeft: () => (
+                            <Link href={'/'} asChild>
+                                <TouchableOpacity>
+                                    <X/>
+                                </TouchableOpacity>
+                            </Link>
+                        ),
+                    }}
+                />
+                <Stack.Screen name="(chat)/[chatuuid]" options={{ headerTitle: 'Chat' }} />
+            </Stack>
+        </ConvexProvider>
+    )
 }
