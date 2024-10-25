ad37d035b6dcb3502fc3243ab4ac568bc4e5fd00
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-10-26 06:26:40 +0800

    Create createGroup.tsx

---
+++ app/(modal)/createGroup.tsx
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import { useRouter } from 'expo-router';
+
+import { Text, KeyboardAvoidingView, Platform, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+import { useMutation } from 'convex/react';
+import { api } from "../../convex/_generated/api";
+
+export default function CreateGroupModal() {
+    const router = useRouter();
+
+    const createGroupChat = useMutation(api.groups.createGroup);
+
+    const [groupName, setGroupName] = useState('');
+    const [groupDescription, setGroupDescription] = useState('');
+    const [groupIcon, setGroupIcon] = useState('');
+
+    const handleGroupCreate = async () => {
+        await createGroupChat({
+            title: groupName,
+            description: groupDescription,
+            icon: groupIcon,
+        });
+
+        router.back();
+    };
+
+    return (
+        <KeyboardAvoidingView
+            style={{
+                flex: 1,
+                backgroundColor: '#F8F5EA',
+                padding: 20,
+            }}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={100}
+        >
+            <Text style={styles.label}>Name</Text>
+            <TextInput style={styles.textInput} value={groupName} onChangeText={setGroupName}/>
+
+            <Text style={styles.label}>Description</Text>
+            <TextInput style={styles.textInput} value={groupDescription} onChangeText={setGroupDescription}/>
+
+            <Text style={styles.label}>Icon URL</Text>
+            <TextInput style={styles.textInput} value={groupIcon} onChangeText={setGroupIcon}/>
+
+            <TouchableOpacity
+                style={{
+                    backgroundColor: '#F9D949',
+                    borderRadius: 5,
+                    padding: 10,
+                    marginVertical: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+                onPress={handleGroupCreate}
+            >
+                <Text
+                    style={{
+                        color: '#3F3F3F',
+                        textAlign: 'center',
+                        fontSize: 16,
+                        fontWeight: 'bold',
+                }}>
+                    Create
+                </Text>
+            </TouchableOpacity>
+        </KeyboardAvoidingView>
+    );
+};
+
+const styles = StyleSheet.create({
+    label: {
+        marginVertical: 10,
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        minHeight: 40,
+        backgroundColor: '#fff',
+    },
+});
