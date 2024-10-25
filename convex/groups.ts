a38675e045e5a56f21f4286a66435d17ee98568e
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-10-26 06:04:53 +0800

    create additional groups helper functions

--- convex/groups.ts
+++ convex/groups.ts
@@ -1,8 +1,26 @@
-import { query } from "./_generated/server";
+import { v } from 'convex/values';
+import { mutation, query } from './_generated/server';
 
-export const get = query({
+export const fetchAllGroups = query({
     args: {},
     handler: async (ctx) => {
         return await ctx.db.query('groups').collect();
-    }
+    },
+});
+
+export const fetchSingleGroup = query({
+    args: { id: v.id('groups') },
+    handler: async (ctx, { id }) => {
+        return await ctx.db
+            .query('groups')
+            .filter((q) => q.eq(q.field('_id'), id))
+            .unique();
+    },
+});
+
+export const createGroup = mutation({
+    args: { description: v.string(), title: v.string(), icon: v.string() },
+    handler: async ({ db }, args) => {
+        await db.insert('groups', args);
+    },
 });
